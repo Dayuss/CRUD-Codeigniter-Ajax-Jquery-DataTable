0c61d3733fcdd0ca17376cea37916a950d46d1fd
Dayuss <ddtardian@gmail.com>
2018-05-25 22:59:41 +0700

    initial

---
+++ application/views/barang/index.php
@@ -0,0 +1,27 @@
+
+	<table class="table table-hover" id="tblBarang">
+		<thead>
+			<tr>
+				<th>No</th>
+				<th>Kode</th>
+				<th>Nama Barang</th>
+				<th>Kategori</th>
+				<th>Tanggal Beli</th>
+				<th>#</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>1</td>
+				<td>S001</td>
+				<td>Monitor LG l300</td>
+				<td>Monitor</td>
+				<td>27/06/2018</td>
+				<td>
+					<a href="#" class="badge badge-primary">Edit</a>
+					<a href="#" class="badge badge-danger">Hapus</a>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+
